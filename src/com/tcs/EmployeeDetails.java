ca74c73bfbd04515fd5416202250191fb46befe0
kavinesh <crazzykavin2002@gmail.com>
2024-06-05 00:20:51 -0700

    code added

---
+++ src/com/tcs/EmployeeDetails.java
@@ -0,0 +1,22 @@
+package com.tcs;
+
+public class EmployeeDetails {
+	private void employeeName() {
+		System.out.println("Employee Name is kavi");
+		
+	}
+	private void employeeLocation() {
+		System.out.println("Employee Location is gobi");
+		
+	}
+	public static void main(String[] args) {
+		EmployeeDetails employee = new EmployeeDetails();
+		employee.employeeName();
+		employee.employeeLocation();
+		
+	}
+		
+	
+
+	
+}
